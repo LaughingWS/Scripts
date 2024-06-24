830056d2d0f9bba2a2b3f5595db844ba176729f6
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-06-23 23:15:32 -0400

    Rework
    No longer teleports to coordinates, now teleports to world location, this gives us X, Y, Z, RX, RY, RZ, W.

---
+++ Creature/City/Thayd/TeleportTo-HalonRing.cs
@@ -0,0 +1,31 @@
+﻿using NexusForever.Shared;
+using NexusForever.Shared.GameTable;
+using NexusForever.Shared.GameTable.Model;
+using NexusForever.WorldServer.Game.Entity;
+using System.Numerics;
+
+namespace NexusForever.WorldServer.Script.Creature.City.Thayd
+{
+    [Script(46183)]
+    public class TeleportTo_Graylight : CreatureScript
+    {
+        const uint WLOC_Graylight = 37777;
+
+        public override void OnActivateSuccess(WorldEntity me, WorldEntity activator)
+        {
+            if (activator is not Player player)
+                return;
+
+            WorldLocation2Entry entry = GameTableManager.Instance.WorldLocation2.GetEntry(WLOC_Graylight);
+            if (entry == null)
+                return;
+
+            if (!player.CanTeleport())
+                return;
+
+            var rotation = new Quaternion(entry.Facing0, entry.Facing1, entry.Facing2, entry.Facing3);
+            player.Rotation = rotation.ToEulerDegrees();
+            player.TeleportTo((ushort)entry.WorldId, entry.Position0, entry.Position1, entry.Position2);
+        }
+    }
+}
---
+++ Creature/City/Thayd/TeleportTo-Malgrave.cs
@@ -0,0 +1,31 @@
+﻿using NexusForever.Shared;
+using NexusForever.Shared.GameTable;
+using NexusForever.Shared.GameTable.Model;
+using NexusForever.WorldServer.Game.Entity;
+using System.Numerics;
+
+namespace NexusForever.WorldServer.Script.Creature.City.Thayd
+{
+    [Script(51207)]
+    public class TeleportTo_Shinysand_Oasis : CreatureScript
+    {
+        const uint WLOC_Shinysand_Oasis = 40707;
+
+        public override void OnActivateSuccess(WorldEntity me, WorldEntity activator)
+        {
+            if (activator is not Player player)
+                return;
+
+            WorldLocation2Entry entry = GameTableManager.Instance.WorldLocation2.GetEntry(WLOC_Shinysand_Oasis);
+            if (entry == null)
+                return;
+
+            if (!player.CanTeleport())
+                return;
+
+            var rotation = new Quaternion(entry.Facing0, entry.Facing1, entry.Facing2, entry.Facing3);
+            player.Rotation = rotation.ToEulerDegrees();
+            player.TeleportTo((ushort)entry.WorldId, entry.Position0, entry.Position1, entry.Position2);
+        }
+    }
+}
