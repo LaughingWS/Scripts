d0324b0212fa0ee7fd0c7f56b781a65c3ca5a4f3
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-06-23 23:18:28 -0400

    Rework
    No longer teleports to coordinates, now teleports to world location, this gives us X, Y, Z, RX, RY, RZ, W.

---
+++ Creature/Transporters/TeleportTo-Illium-Arcterra.cs
@@ -0,0 +1,31 @@
+﻿using NexusForever.Shared;
+using NexusForever.Shared.GameTable;
+using NexusForever.Shared.GameTable.Model;
+using NexusForever.WorldServer.Game.Entity;
+using System.Numerics;
+
+namespace NexusForever.WorldServer.Script.Creature.Transporters
+{
+    [Script(70781)]
+    public class TeleportTo_Illium_Arcterra : CreatureScript
+    {
+        const uint WLOC_Illium_Arcterra = 50187;
+
+        public override void OnActivateSuccess(WorldEntity me, WorldEntity activator)
+        {
+            if (activator is not Player player)
+                return;
+
+            WorldLocation2Entry entry = GameTableManager.Instance.WorldLocation2.GetEntry(WLOC_Illium_Arcterra);
+            if (entry == null)
+                return;
+
+            if (!player.CanTeleport())
+                return;
+
+            var rotation = new Quaternion(entry.Facing0, entry.Facing1, entry.Facing2, entry.Facing3);
+            player.Rotation = rotation.ToEulerDegrees();
+            player.TeleportTo((ushort)entry.WorldId, entry.Position0, entry.Position1, entry.Position2);
+        }
+    }
+}
---
+++ Creature/Transporters/TeleportTo-Thayd-HalonRing.cs
@@ -0,0 +1,31 @@
+﻿using NexusForever.Shared;
+using NexusForever.Shared.GameTable;
+using NexusForever.Shared.GameTable.Model;
+using NexusForever.WorldServer.Game.Entity;
+using System.Numerics;
+
+namespace NexusForever.WorldServer.Script.Creature.Transporters
+{
+    [Script(46185)]
+    public class TeleportTo_Thayd_HalonRing : CreatureScript
+    {
+        const uint WLOC_Thayd_HalonRing = 37772;
+
+        public override void OnActivateSuccess(WorldEntity me, WorldEntity activator)
+        {
+            if (activator is not Player player)
+                return;
+
+            WorldLocation2Entry entry = GameTableManager.Instance.WorldLocation2.GetEntry(WLOC_Thayd_HalonRing);
+            if (entry == null)
+                return;
+
+            if (!player.CanTeleport())
+                return;
+
+            var rotation = new Quaternion(entry.Facing0, entry.Facing1, entry.Facing2, entry.Facing3);
+            player.Rotation = rotation.ToEulerDegrees();
+            player.TeleportTo((ushort)entry.WorldId, entry.Position0, entry.Position1, entry.Position2);
+        }
+    }
+}
