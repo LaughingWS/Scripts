796b6109bc11b5fd8ddadfbcf55f880b7ad58cc5
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-06-23 23:10:05 -0400

    Add files via upload

---
+++ Creature/TeleportTo-Arcterra.cs
@@ -0,0 +1,31 @@
+﻿using NexusForever.Shared;
+using NexusForever.Shared.GameTable;
+using NexusForever.Shared.GameTable.Model;
+using NexusForever.WorldServer.Game.Entity;
+using System.Numerics;
+
+namespace NexusForever.WorldServer.Script.Creature.City.Illium
+{
+    [Script(70783)]
+    public class TeleportTo_Ivory_Hall : CreatureScript
+    {
+        const uint WLOC_Ivory_Hall = 50181;
+
+        public override void OnActivateSuccess(WorldEntity me, WorldEntity activator)
+        {
+            if (activator is not Player player)
+                return;
+
+            WorldLocation2Entry entry = GameTableManager.Instance.WorldLocation2.GetEntry(WLOC_Ivory_Hall);
+            if (entry == null)
+                return;
+
+            if (!player.CanTeleport())
+                return;
+
+            var rotation = new Quaternion(entry.Facing0, entry.Facing1, entry.Facing2, entry.Facing3);
+            player.Rotation = rotation.ToEulerDegrees();
+            player.TeleportTo((ushort)entry.WorldId, entry.Position0, entry.Position1, entry.Position2);
+        }
+    }
+}
