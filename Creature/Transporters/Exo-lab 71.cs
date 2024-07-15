b1038d3d0daff1df9db816f7dcf6c8182d58ecb9
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-07-15 15:11:52 -0400

    7/15/24

---
+++ Creature/Transporters/Exo-lab 71.cs
@@ -0,0 +1,40 @@
+﻿using NexusForever.Shared;
+using NexusForever.Shared.GameTable;
+using NexusForever.Shared.GameTable.Model;
+using NexusForever.WorldServer.Game.Entity;
+using System.Numerics;
+
+namespace NexusForever.WorldServer.Script.Creature.Transporters
+{
+    [Script(28454)]
+    public class TeleportTo_EXOLab_71 : CreatureScript
+{       
+        const uint WLOC_ExoLab_in   = 21760;
+        const uint WLOC_ExoLab_2out = 21761;
+        
+        public override void OnCreate(WorldEntity me)
+        {
+            me.RangeCheck = 2f;
+        }
+
+        public override void OnEnterRange(WorldEntity me, WorldEntity activator)
+        {
+            if (activator is not Player player)
+                return;
+
+            if (!player.CanTeleport())
+                return;
+
+            if (me.Position.Y - 1f > activator.Position.Y)
+                return;
+
+            WorldLocation2Entry entry = GameTableManager.Instance.WorldLocation2.GetEntry(WLOC_ExoLab_in);
+            if (entry == null)
+                return;
+
+                var rotation = new Quaternion(entry.Facing0, entry.Facing1, entry.Facing2, entry.Facing3);
+                player.Rotation = rotation.ToEulerDegrees();
+                player.TeleportTo((ushort)entry.WorldId, entry.Position0, entry.Position1, entry.Position2);
+        }
+    }
+}
---
+++ Creature/Transporters/TeleportTo-Celestion.cs
@@ -0,0 +1,26 @@
+﻿using NexusForever.WorldServer.Game.Entity;
+using System.Numerics;
+
+namespace NexusForever.WorldServer.Script.Creature.Transporters
+{
+    [Script(32269)]
+    public class TeleportTo_Celestion : CreatureScript
+    {
+        const ushort WLOC_Greenbough Glen = 51; // no world location, so this is 1 or 2 X or Z from the real spot
+        private Vector3 LOC_Greenbough Glen = new Vector3(881.554f, -909.42f, -3130.06f);
+        private Vector3 ROT_Greenbough Glen = new Vector3(0f, 0f, 0f);
+
+        public override void OnActivateSuccess(WorldEntity me, WorldEntity activator)
+        {
+            if (activator is not Player player)
+                return;
+
+            if (!player.CanTeleport())
+                return;
+
+            player.Rotation = ROT_Greenbough Glen;
+            player.TeleportTo(WLOC_Greenbough Glen, LOC_Greenbough Glen.X, LOC_Greenbough Glen.Y, LOC_Greenbough Glen.Z);
+
+        }
+    }
+}
