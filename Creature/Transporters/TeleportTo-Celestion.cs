a88f83d01f9056cfb8904a356cd6104497eb56a4
LaughingWS <161174995+LaughingWS@users.noreply.github.com>
2024-08-11 19:34:45 -0400

    Fix for TeleportTo-Celestion

--- Creature/Transporters/TeleportTo-Celestion.cs
+++ Creature/Transporters/TeleportTo-Celestion.cs
@@ -1,4 +1,4 @@
-﻿using NexusForever.WorldServer.Game.Entity;
+using NexusForever.WorldServer.Game.Entity;
 using System.Numerics;
 
 namespace NexusForever.WorldServer.Script.Creature.Transporters
@@ -6,9 +6,9 @@ namespace NexusForever.WorldServer.Script.Creature.Transporters
     [Script(32269)]
     public class TeleportTo_Celestion : CreatureScript
     {
-        const ushort WLOC_Greenbough Glen = 51; // no world location, so this is 1 or 2 X or Z from the real spot
-        private Vector3 LOC_Greenbough Glen = new Vector3(881.554f, -909.42f, -3130.06f);
-        private Vector3 ROT_Greenbough Glen = new Vector3(0f, 0f, 0f);
+        const ushort WLOC_Greenbough_Glen = 51; // no world location, so this is 1 or 2 X or Z from the real spot
+        private Vector3 LOC_Greenbough_Glen = new Vector3(881.554f, -909.42f, -3130.06f);
+        private Vector3 ROT_Greenbough_Glen = new Vector3(0f, 0f, 0f);
 
         public override void OnActivateSuccess(WorldEntity me, WorldEntity activator)
         {
@@ -18,8 +18,8 @@ namespace NexusForever.WorldServer.Script.Creature.Transporters
             if (!player.CanTeleport())
                 return;
 
-            player.Rotation = ROT_Greenbough Glen;
-            player.TeleportTo(WLOC_Greenbough Glen, LOC_Greenbough Glen.X, LOC_Greenbough Glen.Y, LOC_Greenbough Glen.Z);
+            player.Rotation = ROT_Greenbough_Glen;
+            player.TeleportTo(WLOC_Greenbough_Glen, LOC_Greenbough_Glen.X, LOC_Greenbough_Glen.Y, LOC_Greenbough_Glen.Z);
 
         }
     }
